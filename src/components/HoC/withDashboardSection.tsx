0c70120f6a1db1c904fb705e199f00ecfeaa6b78
Violetbee <caglarkarahuseyin28@gmail.com>
2023-03-04 19:19:49 +0300

    removed unnecessary

--- src/components/HoC/withDashboardSection.tsx
+++ src/components/HoC/withDashboardSection.tsx
@@ -24,29 +24,14 @@ export function withDashboardSection(
             {title}
           </span>
           <div className="flex-grow border-t-[1px] border-black"></div>
-          <button className="mx-1 flex-shrink border-2 border-black bg-black py-[1px] px-4 text-lg font-medium text-white md:mx-4 md:px-8 ">
-            {/* Sort Events SVG */}
-            <svg
-              className="h-6 w-6 text-white"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm4 0a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm4 0a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm4 0a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-          <div className="w-2 border-t-[1px] border-black"></div>
 
-          <button className="mx-1 flex-shrink border-2 border-black bg-black py-[1px] px-4 text-lg font-medium text-white after:content-['>'] md:mx-4 md:px-8 md:after:content-['Daha_Fazlasını_Gör'] "></button>
+          <button className="mx-1 flex-shrink rounded-md border-2 border-black bg-[#181823] py-[1px] px-4 text-lg font-medium text-white after:content-['>'] md:mx-4 md:px-8 md:after:content-['Daha_Fazlasını_Gör'] "></button>
           <div className="w-2 border-t-[1px] border-black"></div>
           <button
             onClick={() => {
               setIsExpanded(!isExpanded);
             }}
-            className="mx-1 w-16 flex-shrink border-2 border-black bg-white py-[1px] text-lg font-medium text-black md:mx-4 md:w-20"
+            className="mx-1 w-16 flex-shrink rounded-md border-2 border-black bg-white py-[1px] text-lg font-medium text-black md:mx-4 md:w-20"
           >
             {isExpanded ? "Gizle" : "Göster"}
           </button>
