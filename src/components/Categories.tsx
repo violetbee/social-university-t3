0c0cc6b59528347a5bb7a1cec83666e554b5fc23
Violetbee <caglarkarahuseyin28@gmail.com>
2023-04-30 16:56:23 +0300

    category section hover ok

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,31 +1,32 @@
 import Link from "next/link";
-import { useState, useRef, createRef } from "react";
+import { useState } from "react";
 import { trpc } from "../utils/trpc";
 import { IDashboardProps } from "../types/app";
 
 const Categories = ({ params }: IDashboardProps) => {
   const { data } = trpc.category.getAll.useQuery();
-  const linkRef = useRef(null);
+  const [selectedCategory, setSelectedCategory] = useState<string>(
+    params?.category || "her-sey"
+  );
 
   return (
     <div className="flex items-center">
       {data &&
         data.map((category) => (
           <Link
-            ref={linkRef}
             id={category.slug}
-            onMouseEnter={() => {
-              console.log(document.getElementById(linkRef.current));
-            }}
             href={
               category.slug === "her-sey"
                 ? "/dashboard"
                 : `/gonderiler/${category.slug}`
             }
+            onMouseEnter={() => setSelectedCategory(category.slug)}
+            onMouseLeave={() =>
+              setSelectedCategory(params?.category || "her-sey")
+            }
             key={category.id}
-            className={`flex-shrink-0 cursor-pointer whitespace-nowrap border-[1px] px-3 py-2 align-middle leading-[1.2] duration-150  ${
-              category.slug === params?.category ||
-              (category.slug === "her-sey" && !params?.category)
+            className={`flex-shrink-0 cursor-pointer whitespace-nowrap border-[1px] px-3 py-2 align-middle leading-[1.2] duration-150 ${
+              selectedCategory === category.slug
                 ? "rounded-full border-[#888] bg-white hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[0.25rem_0.25rem_#333]"
                 : "border-transparent"
             }`}
--- src/components/CategoryAndMenuSection.tsx
+++ src/components/CategoryAndMenuSection.tsx
@@ -9,7 +9,7 @@ const CategoryAndMenuSection = ({ params }: IDashboardProps) => {
   const ShareBt = withPopup(ShareButton, ShareFormMain);
 
   return (
-    <div className="flex flex-col gap-4 px-5 pt-6 pb-3">
+    <div className="flex flex-col gap-4 px-5 pb-3 pt-6">
       <div className="flex items-center justify-between gap-4">
         <SearchInput />
         <ShareBt />
